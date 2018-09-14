8d5f9819253db854a9bd714b2df00b530101a205
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-09-14 10:57:32 -0500

    first commit

---
+++ test/bramipsum_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class BramipsumTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Bramipsum::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
